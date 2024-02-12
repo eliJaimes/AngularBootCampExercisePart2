2bc7e9fd6ec42d745f9e94f90b17417a38e58f4d
Eli Jaimes <eli.jaimes.ramos@gmail.com>
2024-02-12 11:54:23 -0600

    Avoid subscribing on the controller

--- NOTES.md
+++ NOTES.md
@@ -5,3 +5,11 @@
 # Change post service
 
 - Refactor the service to expose an observable property instead of a method, this will allow to use a declarative approach instead of a procedural one
+
+# Avoid subscribing on the controller
+
+- Refactor PostsTableComponent so instead of subscribing to the posts$ observable in the controller, we will subscribe in the view, using the async pipe
+
+- The async pipe will handle the subscription/un subscription for us
+
+- Note: this change will break filtering mechanism
--- src/app/components/posts-table/posts-table.component.ts
+++ src/app/components/posts-table/posts-table.component.ts
@@ -1,45 +1,37 @@
 /* ••[1]••••••••••••••••••••••••• posts-table.component.ts •••••••••••••••••••••••••••••• */
 
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { Post } from '../../entities/post.type';
 import { PostsService } from '../../services/posts.service';
-import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-posts-table',
 	styleUrl: './posts-table.component.scss',
 	templateUrl: './posts-table.component.html',
 })
-export class PostsTableComponent implements OnInit, OnDestroy {
+export class PostsTableComponent implements OnInit {
 	private posts: Array<Post> = [];
 
 	public filteredPosts: Array<Post> = [];
 
-	private postsSubscription!: Subscription;
+	private posts$!: Observable<Array<Post>>;
+
+	public filteredPosts$!: Observable<Array<Post>>;
 
 	public constructor(private readonly postsService: PostsService) {}
 
-	/* NOTE: this method will be executed by Angular on component's initialization */
 	public ngOnInit(): void {
-		this.postsSubscription = this.postsService.posts$.subscribe({
-			error: (error: Error): void => {
+		this.posts$ = this.postsService.posts$.pipe(
+			catchError((error: Error): Observable<Array<Post>> => {
 				console.error('❌ - Something wrong occurred: %O', error);
-				this.posts = [];
-			},
-			next: (posts: Array<Post>): void => {
-				this.filteredPosts = this.posts = posts;
-				console.log('this.posts: %O', this.posts);
-			},
-		});
+				return of([]);
+			}),
+		);
+
+		this.filteredPosts$ = this.posts$;
 	}
 
-	/* NOTE: this method will be executed by Angular on component's destruction */
-	public ngOnDestroy(): void {
-		this.postsSubscription?.unsubscribe();
-	}
-
-	/* NOTE: filtering logic should be handle it on the component responsible of
-	managing the posts */
 	public filterChangeHandler(event: string): void {
 		const filterValue: string = event.toLowerCase();
 
